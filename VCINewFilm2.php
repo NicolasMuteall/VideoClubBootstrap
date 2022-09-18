93f700db3cdd1f772ecefa927b361bb40c0ebb66
NicolasDpnt <nicolas.dupont59176@gmail.com>
2022-09-18 18:57:41 +0200

    UPDATED files

--- VCINewFilm2.php
+++ VCINewFilm2.php
@@ -4,9 +4,8 @@
 ?>
 
 <div class='container border mt-5 text-center'>
-    <h1>Saisie d'un nouveau film :</h1>
+    <h1 class='mb-3'>Saisie d'un nouveau film :</h1>
     <?php
-            var_dump($_POST);
             $titre = $_POST['titre'];
             $type = $_POST['choixtype'];
             $real = $_POST['choixreal'];
@@ -21,12 +20,12 @@
             //var_dump($results);
 
             if(!preg_match('/./', $titre)){
-                echo "<h2>Le titre doit être renseigné.</h2><br>";
+                echo "<h5>Le titre doit être renseigné.</h5><br>";
                 $error = true;
             }
             if(!preg_match('/^[0-9]{4}$/', $annee) || !preg_match('/./', $annee)){
                 //Si c'est pas des chiffres ou s'il y a plus de 4 chiffres ou si c'est vide :
-                echo "<h2>L'année doit être renseignée et le format doit être correct.</h2><br>";
+                echo "<h5>L'année doit être renseignée et le format doit être correct.</h5><br>";
                 $error = true;
             }else{
                 $error = false;
@@ -37,34 +36,37 @@
                 $sizeFile = $_FILES['affiche']['size'];
                 $tmpFile = $_FILES['affiche']['tmp_name'];
                 $errFile = $_FILES['affiche']['error'];
-                var_dump($nameFile);
         
                 $extensions = ['png', 'jpg', 'jpeg'];
-                $type = ['image/png', 'image/jpg', 'image/jpeg'];
+                $typeimg = ['image/png', 'image/jpg', 'image/jpeg'];
         
                 $extension = explode('.', $nameFile);
         
                 $max_size = 100000;
 
-                if(in_array($typeFile, $type)) {
-                    if(move_uploaded_file($tmpFile, 'C:\wamp64\www\EX_PHP\VideoBootstrap\pictures\AddFilms\ ' .uniqid() . '.' . strtolower(end($extension) ) ) ){ 
-                        echo "This is uploaded!";
-                    }else{ 
-                        echo "failed";
-                    }
-                }else{
-                    echo "Type non autorisé.";
+                if(!in_array($typeFile, $typeimg)) {
+                    echo "<h5>Type de ficher non autorisé.</h5><br>";
+                    $error = true;
                 }
             }
+            if($_FILES['affiche']['size'] === 0){
+                echo "<h5>Une image doit-être ajoutée.</h5><br>";
+                $error = true;
+            }
             if(!$error){ 
-                $sql = "INSERT into film (TITRE_FILM, ANNEE_FILM, ID_REALIS, CODE_TYPE_FILM)
-                VALUES ('$titre', '$annee', '$real', '$type')";
+                move_uploaded_file($tmpFile, 'C:\wamp64\www\EX_PHP\VideoBootstrap\pictures\AddFilms\ ' .uniqid() . '.' . strtolower(end($extension) ) );
+
+                $sql = "INSERT into film (TITRE_FILM, ANNEE_FILM, ID_REALIS, CODE_TYPE_FILM, RESUME, REF_IMAGE)
+                VALUES ('$titre', '$annee', '$real', '$type', '$resume', '$nameFile')";
                 $cnx->exec($sql);
                 /*Si suppression d'un film, il faut faire la requête "ALTER TABLE 'nom de la table' AUTO_INCREMENT=0;" pour
                 reinitialiser l'auto-increment*/
                 echo "<h2>Film ajouté avec succès !</h2><br>";
             }
-        ?>
+    ?>
+    <div class="mt-2 mb-2">
+        <button type="button" onclick="history.go(-1);" class="btn btn-secondary">Retour</button>
+    </div>
 </div>    
 <?php 
     include('VCIFooter.php');
